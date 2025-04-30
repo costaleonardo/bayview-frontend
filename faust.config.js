b4ecac5529e817ef418d313813330bd36d903574
Leonardo Da Costa <costaleonardo97@gmail.com>
2025-04-30 07:41:35 -0400

    fix deploy errors

--- faust.config.js
+++ faust.config.js
@@ -1,4 +1,4 @@
-import templates from "./src/wp-templates/index";
+import templates from "./src/wp-templates/index.js"; // Explicit .js extension
 import possibleTypes from "./possibleTypes.json";
 import { setConfig } from "@faustwp/core";
 
@@ -8,4 +8,7 @@ import { setConfig } from "@faustwp/core";
 export default setConfig({
   templates,
   possibleTypes,
+  wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL, // Add WordPress URL
+  apiClientSecret: process.env.FAUSTWP_SECRET_KEY, // Optional, for previews
+  revalidate: 900, // 15 minutes revalidation for ISR
 });
